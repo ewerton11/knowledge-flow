dd8ae56a3626abec74c2cdf69c3b713d40dd508d
ewerton11 <ewertonreisc01@gmail.com>
2023-05-24 12:00:26 -0500

    child topic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,39 +7,48 @@ import { useState, useEffect } from 'react'
 import api from '@/lib/axios'
 import NavTopic from '@/components/navTopic'
 
-interface ITopic {
+export interface ITopic {
   id: number
   title: string
+  parentId: null | number
 }
 
 export default function Home() {
   const [data, setData] = useState<ITopic[]>([])
-  const [topicInfoId, setTopicInfoId] = useState<number | null>(null)
   const [selectedTopicId, setSelectedTopicId] = useState<number | null>(null)
+
+  // const [topicInfoId, setTopicInfoId] = useState<number | null>(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await api.get('/')
-        const responseData = response.data
-        setData(responseData)
+        if (selectedTopicId === null) {
+          const response = await api.get('/topics')
+          const responseData = response.data
+          setData(responseData)
+        } else {
+          const responseChild = await api.get(
+            `/topics/${selectedTopicId}/child`
+          )
+          const responseDataChild = responseChild.data
 
-        //pegar o id especifico do topico selecionado e armazenar em um estado os dados dele
+          setData(responseDataChild)
+        }
       } catch (error) {
         console.error(error)
       }
     }
 
     fetchData()
-  }, [])
+  }, [selectedTopicId])
 
-  const handleTopicClick = (topicId: number) => {
-    if (topicInfoId === topicId) {
-      setTopicInfoId(null)
-    } else {
-      setTopicInfoId(topicId)
-    }
-  }
+  // const handleTopicClick = (topicId: number) => {
+  //   if (topicInfoId === topicId) {
+  //     setTopicInfoId(null)
+  //   } else {
+  //     setTopicInfoId(topicId)
+  //   }
+  // }
 
   const handleAddTopic = (topicId: number) => {
     setSelectedTopicId(topicId)
@@ -55,50 +64,48 @@ export default function Home() {
         </div>
         {data.map((topic: ITopic) => {
           return (
-            <>
-              <div
-                key={topic.id}
-                className="w-4/5 h-16 mt-10 bg-white relative rounded-2xl border border-gray-300 shadow-sm flex justify-between"
-              >
-                <div className="w-10 h-10 flex justify-center items-center absolute left-3 -translate-y-1/2">
-                  <Image
-                    src={Idea}
-                    width={300}
-                    height={300}
-                    alt="Image of an idea"
-                    className="w-2/3 h-2/3"
-                  />
-                </div>
-                <div className="w-[13%] flex items-center justify-center">
-                  <p
-                    className="text-gray-500 font-sans text-2xl cursor-pointer"
-                    onClick={() => handleAddTopic(topic.id)}
-                  >
-                    +
-                  </p>
-                </div>
-                <div
-                  className="w-[67%] flex items-center text-blue-600 font-sans cursor-pointer"
-                  onClick={() => handleTopicClick(topic.id)}
-                >
-                  <p>{topic.title}</p>
-                </div>
-                <div className="w-1/5 flex items-center text-gray-500 font-sans">
-                  <p>12/05/2023</p>
-                </div>
+            <div
+              key={topic.id}
+              className="w-4/5 h-16 mt-10 bg-white relative rounded-2xl border border-gray-300 shadow-sm flex justify-between"
+            >
+              <div className="w-10 h-10 flex justify-center items-center absolute left-3 -translate-y-1/2">
+                <Image
+                  src={Idea}
+                  width={300}
+                  height={300}
+                  alt="Image of an idea"
+                  className="w-2/3 h-2/3"
+                />
               </div>
-              {topicInfoId === topic.id && (
-                <div className="w-[65%] border-l-4 border-gray-400">
-                  <p className="p-5 font-sans">
-                    <span className="text-blue-800">(22)</span> Sub-tarefas
-                  </p>
-                  <p className="p-5 font-sans">
-                    <span className="text-blue-800">(5/22)</span> Sub-tarefas
-                    concluidas
-                  </p>
-                </div>
-              )}
-            </>
+              <div className="w-[13%] flex items-center justify-center">
+                <p
+                  className="text-gray-500 font-sans text-2xl cursor-pointer"
+                  onClick={() => handleAddTopic(topic.id)}
+                >
+                  +
+                </p>
+              </div>
+              <div
+                className="w-[67%] flex items-center text-blue-600 font-sans cursor-pointer"
+                onClick={() => handleTopicClick(topic.id)}
+              >
+                <p>{topic.title}</p>
+              </div>
+              <div className="w-1/5 flex items-center text-gray-500 font-sans">
+                <p>12/05/2023</p>
+              </div>
+            </div>
+            // {topicInfoId === topic.id && (
+            //   <div className="w-[65%] border-l-4 border-gray-400">
+            //     <p className="p-5 font-sans">
+            //       <span className="text-blue-800">(22)</span> Sub-tarefas
+            //     </p>
+            //     <p className="p-5 font-sans">
+            //       <span className="text-blue-800">(5/22)</span> Sub-tarefas
+            //       concluidas
+            //     </p>
+            //   </div>
+            // )}
           )
         })}
       </main>
--- src/components/navTopic/index.tsx
+++ src/components/navTopic/index.tsx
@@ -1,17 +1,39 @@
+import { ITopic } from '@/app/page'
+import api from '@/lib/axios'
+import { useEffect, useState } from 'react'
+
 interface ISelectedTopicId {
   selectedTopicId: number | null
 }
 
 export default function NavTopic({ selectedTopicId }: ISelectedTopicId) {
+  const [data, setData] = useState<ITopic | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (selectedTopicId !== null) {
+          const response = await api.get(`/topics/${selectedTopicId}`)
+          const responseData = response.data
+          setData(responseData)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchData()
+  }, [selectedTopicId])
+
   return (
-    <div className="w-[5%] h-full bg-blue-400 fixed left-[20%] flex justify-center items-center">
-      {selectedTopicId === null ? (
-        <h1 className="[writingMode:vertical-rl] [textOrientation:upright] bg-yellow-400 font-sans text-lg">
-          inicio {selectedTopicId}
+    <div className="w-[5%] h-full border-l fixed left-[20%] flex justify-center items-center">
+      {data === null ? (
+        <h1 className="[writingMode:vertical-rl] [textOrientation:upright] font-sans text-lg">
+          inicio
         </h1>
       ) : (
-        <h1 className="[writingMode:vertical-rl] [textOrientation:upright] bg-yellow-400 font-sans text-lg">
-          exemplo {selectedTopicId}
+        <h1 className="[writingMode:vertical-rl] [textOrientation:upright] font-sans text-lg">
+          {data.title}
         </h1>
       )}
     </div>
